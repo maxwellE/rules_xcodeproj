c993a6bb59d53cf772fc0ab925fba77e0418200e
Brentley Jones <github@brentleyjones.com>
2022-04-15 19:53:50 -0500

    WIP

--- examples/ios_app/Example/ContentView.swift
+++ examples/ios_app/Example/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Text("\(Foo().greeting())\n\(Bar().baz())\n\(Baz.bar)")
+            Text("4\(Foo().greeting())\n\(Bar().baz())\n\(Baz.bar)")
                 .padding()
             Image("rules_xcodeproj", bundle: Self.exampleResourcesBundle)
                 .resizable()
